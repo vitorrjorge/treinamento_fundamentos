8112e40dc79371a06fb74b0345cb574a763c004e
Vitor Jorge <vitorj31@hotmail.com>
2021-02-19 13:01:03 -0300

    Aula 2

---
+++ cadastro.rb
@@ -0,0 +1,7 @@
+print "Digite o seu nome: "
+nome = gets.chomp
+
+print "Digite a sua idade: "
+idade = gets.chomp
+
+puts "Seu nome é #{nome} e sua idade é #{idade}"
---
+++ calc.rb
@@ -0,0 +1,11 @@
+print "Digite um numero: "
+num1 = gets.chomp.to_i
+
+print "Digite outro numero: "
+num2 = gets.chomp.to_i
+
+soma = num1 + num2
+subt = num1 - num2
+divi = num1 / num2
+
+puts "A adição é: #{num1}+#{num2}; A soma é: #{soma}; A subtração é: #{subt}; Adivisão é: #{divi}"
